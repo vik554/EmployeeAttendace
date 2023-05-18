6c9dada2f0da85b1ac7d2b86e0d70aa61699d417
vik554 <kumbharvikram27@gmail.com>
2023-05-18 13:47:25 +0530

    ADDED: code to employee attendace

--- src/com/bl/emp_wage/EmpWage.java
+++ src/com/bl/emp_wage/EmpWage.java
@@ -1,8 +1,18 @@
 package com.bl.emp_wage;
 
+
+
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        //Employee Attendance UC-1
+        int att = (int)(Math.random() * 10) % 2;
+        if (att==1){
+            System.out.println("Employee is Present");
+        }else{
+            System.out.println("Employee is Absent");
+        }
 
     }
+
 }
